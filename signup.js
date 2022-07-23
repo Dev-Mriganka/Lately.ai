ad411ac6e08150e68aa4067f2373a6734e4b4a6c
1oneWalker <mrigankamondal10@gmail.com>
2022-07-23 06:06:16 +0530

    almost 90%

--- signup.js
+++ signup.js
@@ -2,16 +2,38 @@ document.getElementById("sign_btn").addEventListener("click",signup);
 
 function signup()
 {
-    document.querySelector("body").innerHTML=null;
-    let image=document.createElement("img");
-    image.setAttribute("id","buffering_image")
-    image.src = "https://c.tenor.com/zXFBtV7hHR8AAAAM/loading-buffering.gif";
-    setTimeout(function () {
+   let side = document.querySelector("#side_panel");
+   side.classList.toggle("open")
+   let black = document.querySelector(".side-nav-overlay")
+   black.classList.toggle("overlay-active")
+   let disable = document.querySelector("#full_body")
+   disable.classList.toggle("disable")
+   setTimeout(() => {
+      let x = document.querySelector(".side-nav-overlay img")
+      .src = "";
+   }, 2000)
+//     let image=document.createElement("img");
+//     image.setAttribute("id","buffering_image")
+//     image.src = "https://c.tenor.com/zXFBtV7hHR8AAAAM/loading-buffering.gif";
+//     setTimeout(function () {
 
-        window.location.href="function.html";
+//         window.location.href="function.html";
        
-    }, 3000)
+//     }, 3000)
 
-    document.querySelector("body").append(image);
+//     document.querySelector("body").append(image);
    
+}
+document.getElementById("cross").addEventListener("click", close);
+function close() {
+   let side = document.querySelector("#side_panel");
+   side.classList.toggle("open")
+   let black = document.querySelector(".side-nav-overlay")
+   black.classList.toggle("overlay-active")
+   let disable = document.querySelector("#full_body")
+   disable.classList.toggle("disable")
+   setTimeout(() => {
+      let x = document.querySelector(".side-nav-overlay img")
+         .src = "./giphy.gif";
+   }, 2000)
 }
